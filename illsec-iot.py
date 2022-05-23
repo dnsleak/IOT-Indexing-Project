e5336058c680ddbdfca02e1f24275aa519e42dcf
iLLSeC <34897992+dnsleak@users.noreply.github.com>
2022-05-23 07:20:01 -0400

    Update illsec-iot.py

--- illsec-iot.py
+++ illsec-iot.py
@@ -1,11 +1,1 @@
-#usage is python2 illsec-iot.py iplist.txt portnumber. it auto outputs the data into a file named as the ip its making a request to.
-import sys, os
-
-port = int(sys.argv[2])
-ips = open(sys.argv[1], "r").readlines()
-
-for ip in ips:
-    ip = ip.strip("\r\n")
-    ip = ip+":"+str(port)
-    cmd = "curl -I -m 6 '%s' >> '%s'" % (ip, ip) #default curl request as of right now, working on setting some other juicy hooks up.
-    os.system(cmd)
+***REDACTED***
